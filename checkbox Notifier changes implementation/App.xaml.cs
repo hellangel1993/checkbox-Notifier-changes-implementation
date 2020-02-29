affea247aa12f9ec7d89ca1f6eb198c0817c187c
Himanshu Chowdhary <himanshu.chowdhary@qvantel.com>
2020-02-29 21:35:40 +0530

    Add project files.

---
+++ checkbox Notifier changes implementation/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace checkbox_Notifier_changes_implementation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
